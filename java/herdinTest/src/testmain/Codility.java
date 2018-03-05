ef6c157344b219a788b5bc5f5d60c15677278bd5
herdin <herdin86@gmail.com>
2018-03-06 02:08:19 +0900

    home PC commit

--- java/herdinTest/src/testmain/Codility.java
+++ java/herdinTest/src/testmain/Codility.java
@@ -1,66 +1,19 @@
 package testmain;
 
-public class Codility {
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 
+public class Codility {
+	
 	public static void main(String[] args) {
 //		Codility.Ferrum_2018();
-		Codility.Cobaltum_2018();
+//		Codility.Cobaltum_2018();
+		Codility.Manganum_2017();
 	}//END OF FUNCTION
 	
 	public static void Cobaltum_2018() {
-		int[] A = {5, 3, 7, 7, 10};
-		int[] B = {1, 6, 6, 9, 9};
-		
-		for(int a : A)
-			System.out.print(a + " ");
-		System.out.println();
-		for(int b : B)
-			System.out.print(b + " ");
-		System.out.println();
-//		int[] diffA = new int[A.length];
-//		int[] diffB = new int[A.length];
-//		for(int i=0; i<A.length; i++) {
-//			if(i==0) {
-//				diffA[i] = A[i];
-//				diffB[i] = B[i];
-//			} else {
-//				diffA[i] = A[i]-A[i-1];
-//				diffB[i] = B[i]-B[i-1];
-//			}
-//		}
-//		
-//		for(int a : diffA)
-//			System.out.print(a + " ");
-//		System.out.println();
-//		for(int b : diffB)
-//			System.out.print(b + " ");
-//		System.out.println();
-		int swapCnt = 0;
-		for(int i=0; i<A.length; i++) {
-			if(
-				(i<=A.length-2) &&
-				(	(	A[i]>=A[i+1] && A[i]<B[i+1] && B[i]<A[i+1] &&
-						(0<A.length && A[i-1]<B[i]) &&
-						(i<=A.length-3 && )
-					)
-					||
-					(	B[i] >= B[i+1] && B[i] < A[i+1] && A[i] < B[i+1] &&
-					)
-				)
-			){
-				int tmp = A[i];
-				A[i] = B[i];
-				B[i] = tmp;
-				swapCnt++;
-			}
-		}
-		System.out.println(swapCnt);
-		for(int a : A)
-			System.out.print(a + " ");
-		System.out.println();
-		for(int b : B)
-			System.out.print(b + " ");
-		System.out.println();
 	}//END OF FUNCTION
 	
 	public static void Ferrum_2018() {
@@ -102,5 +55,121 @@ public class Codility {
 //			}
 //		}
 		System.out.println(maxLength);
+	}//END OF FUNCTION
+	
+	public static int Manganum_2017() {
+//		int[] X = {};
+//		int[] Y = {};
+//		String T = "";
+//		int[] X = {3, 5, 1, 6};
+//		int[] Y = {1, 3, 3, 8};
+//		String T = "Xpqp"; // return 10
+//		int[] X = {0, 3, 5, 1, 6};
+//		int[] Y = {4, 1, 3, 3, 8};
+//		String T = "pXpqp"; // return 2
+		int[] X = {0, 6, 2, 5, 3, 0};
+		int[] Y = {4, 8, 2, 3, 1, 6};
+		String T = "ppqpXp"; // return 12
+		
+		boolean isLocal = true;
+		int maxScore = -1;
+		char[] Tch = T.toCharArray();
+		if(X.length != Y.length || X.length != Tch.length) return maxScore;
+		
+		if(isLocal) {
+			System.out.print("X   : ");
+			for(int x : X)
+				System.out.print(x + " ");
+			System.out.println();
+			System.out.print("Y   : ");
+			for(int y : Y)
+				System.out.print(y + " ");
+			System.out.println();
+			System.out.print("Tch : ");
+			for(char ch : Tch)
+				System.out.print(ch + " ");
+			System.out.println();
+		}
+		
+		HashMap<Integer, ArrayList<Integer>> hashCoord = new HashMap<>();
+		int curX = 0;
+		int curY = 0;
+		for(int i=0; i<X.length; i++) {
+			if(Tch[i] == 'X') {
+				curX = X[i];
+				curY = Y[i];
+			} else if(hashCoord.get(Integer.valueOf(X[i])) == null) {
+				ArrayList<Integer> ys = new ArrayList<>();
+				ys.add(Integer.valueOf(Y[i]));
+				hashCoord.put(Integer.valueOf(X[i]), ys);
+			} else {
+				ArrayList<Integer> ys = hashCoord.get(Integer.valueOf(X[i]));
+				ys.add(Integer.valueOf(Y[i]));
+			}
+		}
+		
+		if(isLocal) {
+			System.out.println("curX, curY : " + curX + ", " + curY);
+			System.out.println("hashMap : ");
+			Iterator iter = hashCoord.keySet().iterator();
+			while(iter.hasNext()) {
+				Integer key = (Integer) iter.next();
+				System.out.println(key + " : " + hashCoord.get(key));
+			}
+		}
+		 
+		class littleSolution {
+			private int[] X;
+			private int[] Y;
+			private char[] Tch;
+			public littleSolution(int[] X, int[] Y, char[] Tch) {
+				this.X = X;
+				this.Y = Y;
+				this.Tch = Tch;
+			}//END OF FUNCTION
+			
+			public int getMaxScore(int curX, int curY, boolean isCoordFixed, boolean isLeft) {
+				//fixed up-left  : x1-n, y1+n = x3, y3 : x1+y1 = x3+y3
+				//fixed up-right : x1+n, y1+n = x2, y2 : n = {(x2+y2)-(x1+y1)}/2
+				//up-left  : (x1-n, y1+n) = (x3-m, y3-m) : n = {(x1-y1)-(x3-y3)}/2
+				//up-right : (x1+n, y1+n) = (x2+m, y2-m) : n = {(x2+y2)-(x1+y1)}/2
+				//point that middle of meetpoint will be ignored. cuz obviously smaller.
+				int maxScore = 0;
+				
+				for(int i=0; i<X.length; i++) {
+					int tmpScore = 0;
+					
+					if(Tch[i] != 'X' && )
+					
+					
+					
+					
+					if(tmpScore > maxScore) {
+						maxScore = tmpScore;
+					}
+				}//END OF FOR-LOOP
+				
+				return maxScore;
+			}//END OF FUNCTION
+			
+			public boolean isValidCoordinate(int testX, int testY) {
+				boolean validity = true;
+				ArrayList<Integer> ys = hashCoord.get(Integer.valueOf(testX));
+				if(ys != null) {
+					for(Integer y : ys) {
+						if(y.equals(Integer.valueOf(testY))) {
+							validity = false;
+							break;
+						}
+					}
+				}
+				return validity;
+			}
+		}//END OF INNER-CLASS
+		
+		
+		System.out.println("max score : " + new littleSolution(X, Y, Tch).getMaxScore(curX, curY, true));
+		
+		return maxScore;
 	}//END OF FUNCTION
 }//END OF CLASS
